f982d6744e093d2be01a70390c432fb95b7db7d6
NieDongjia <327803131@qq.com>
2020-02-04 15:33:41 +0800

    <feat>:< 粘包问题探索与深入>

---
+++ src/main/java/com/djn/cn/op/netty/base/demo/v2/TimeServerHandler.java
@@ -0,0 +1,47 @@
+package com.djn.cn.op.netty.base.demo.v2;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.Date;
+
+/**
+ * <b>类   名：</b>com.djn.cn.op.netty.base.demo.TimeServerHandler<br/>
+ * <b>类描述：</b> 未考虑TCP 粘包 Demo  <br/>
+ * <b>创建人：</b>op.nie-dongjia<br/>
+ * <b>创建时间：</b>2020/2/3 17:07<br/>
+ * <b>修改人：</b>op.nie-dongjia<br/>
+ * <b>修改时间：</b>2020/2/3 17:07<br/>
+ * <b>修改备注：</b><br/>
+ *
+ * @version 1.0<br/>
+ */
+public class TimeServerHandler extends ChannelHandlerAdapter {
+    private int counter;
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+            throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8").
+                substring(0,req.length-System.getProperty("line.separator").length());
+        System.out.println("The time server receive order:" + body
+        + "; the counter is ：" + ++counter);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)?
+                new Date(System.currentTimeMillis()).toString() : "BAD ORDER ";
+
+        currentTime = currentTime +  System.getProperty("line.separator");
+
+
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/djn/cn/op/netty/base/demo/v3/TimeClient.java
@@ -0,0 +1,64 @@
+package com.djn.cn.op.netty.base.demo.v3;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * <b>类   名：</b>com.djn.cn.op.netty.base.demo.TimeClient<br/>
+ * <b>类描述：</b>TODO<br/>
+ * <b>创建人：</b>op.nie-dongjia<br/>
+ * <b>创建时间：</b>2020/2/3 21:17<br/>
+ * <b>修改人：</b>op.nie-dongjia<br/>
+ * <b>修改时间：</b>2020/2/3 21:17<br/>
+ * <b>修改备注：</b><br/>
+ *
+ * @version 1.0<br />
+ */
+public class TimeClient {
+    public void connect(int port,String host) throws Exception{
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(eventLoopGroup).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY,true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                            ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+
+            ChannelFuture channelFuture = b.connect(host,port).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if(args != null && args.length > 0){
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                // 默认 接口
+            }
+        }
+
+        new TimeClient().connect(port,"127.0.0.1");
+
+    }
+}
