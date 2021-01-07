0e706cdb677a30ec9996ce5f67102f34b331ac7a
nie-dongjia <niedongjiamail@qq.com>
2021-01-07 10:24:51 +0800

    <fix>:<测试文档>

--- src/main/java/com/djn/cn/op/netty/base/demo/v7/ZTBrokenClient.java
+++ src/main/java/com/djn/cn/op/netty/base/demo/v7/ZTBrokenClient.java
@@ -8,6 +8,8 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 
 /**
  * <b>类   名：</b>com.djn.cn.op.netty.base.demo.ZTBrokenClient<br/>
@@ -31,6 +33,8 @@ public class ZTBrokenClient {
                     .handler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                            ch.pipeline().addLast(new StringDecoder());
                             ch.pipeline().addLast(new ZTBrokenClientHandler());
                         }
                     });
